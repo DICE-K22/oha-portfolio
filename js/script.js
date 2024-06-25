7369e1227075157f84bbe94c78bbbac36edaff03
Daisuke <72855911+DICE-K22@users.noreply.github.com>
2024-06-25 15:56:57 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
+
+jQuery(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+
+  if (jQuery(this).parent().hasClass("is-open")) {
+    jQuery(this).parent().removeClass("is-open");
+    jQuery(this).next().slideUp();
+  } else {
+    jQuery(this).parent().addClass("is-open");
+    jQuery(this).next().slideDown();
+  }
+});
+
+const swiper = new Swiper("#js-gallery-swiper", {
+  spaceBetween: 82,
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: "#js-gallery-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#js-gallery-next",
+    prevEl: "#js-gallery-prev",
+  },
+});
+
+jQuery(".js-modal-open").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].showModal();
+});
+
+jQuery(".js-modal-close").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].close();
+});
